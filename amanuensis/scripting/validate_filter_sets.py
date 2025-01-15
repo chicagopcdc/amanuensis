668ae45013510c72d131f5da7ac5c81940380a1b
paulmurdoch19 <paulmurdoch19@gmail.com>
2025-01-15 11:43:23 -0800

    add parameter to support a specific config file

--- amanuensis/scripting/validate_filter_sets.py
+++ amanuensis/scripting/validate_filter_sets.py
@@ -1,16 +1,18 @@
-
+import argparse
 from cdislogging import get_logger
 from userportaldatamodel.driver import SQLAlchemyDriver
 from amanuensis.settings import CONFIG_SEARCH_FOLDERS
 from amanuensis.config import config
 from amanuensis.resources.filter_sets import check_filter_sets
+
 logger = get_logger(__name__, log_level="info")
 
 
 
-def main():
+def main(config_file_name=None):
     config.load(
-        search_folders=CONFIG_SEARCH_FOLDERS
+        search_folders=CONFIG_SEARCH_FOLDERS,
+        file_name=config_file_name
     )
     SQLAlchemyDriver.setup_db = lambda _: None
     db = SQLAlchemyDriver(config["DB"])
@@ -22,4 +24,12 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    # Set up command-line argument parsing
+    parser = argparse.ArgumentParser(description="Run the filter sets checker.")
+    parser.add_argument(
+        "--file_name",
+        help="Optional configuration file name to load.",
+        default=None
+    )
+    args = parser.parse_args()
+    main(config_file_name=args.file_name)
