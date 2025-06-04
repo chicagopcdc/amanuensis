477cc01dd121a20a71f6add850e662e2e7094bf5
grugna <graglia01@gmail.com>
2025-06-04 12:30:07 -0500

    remove logs

--- amanuensis/resources/fence/__init__.py
+++ amanuensis/resources/fence/__init__.py
@@ -53,17 +53,12 @@ def fence_get_users(usernames=None, ids=None):
             body=body
         )
 
-        logger.error("AAAAAAAAA")
-        logger.error(payload.get_standardized_payload(config.get("SERVICE_NAME").upper()))
-
         g3rm = Gen3RequestManager(headers=headers)
 
         signature = g3rm.make_gen3_signature(payload, config=config)
         headers["Content-Type"] = "application/json"
         headers["Authorization"] = "bearer " + jwt
         headers["Signature"] = "signature " + signature
-
-        logger.error("signature " + signature)
 
         r = requests.post(url, data=body, headers=headers)
         if r.status_code == 200:
