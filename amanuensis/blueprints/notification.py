cc0751fa7aa8cfb2de4ded1036389e904321f90d
Luca Graglia <graglia01@gmail.com>
2025-07-02 10:20:50 -0500

    addget all notification log

--- amanuensis/blueprints/notification.py
+++ amanuensis/blueprints/notification.py
@@ -2,8 +2,8 @@ import flask
 
 from amanuensis.auth.auth import current_user
 from amanuensis.errors import AuthError
-from amanuensis.schema import NotificationSchema
-from amanuensis.resources.notification import update_users_notifications, get_all_notifications
+from amanuensis.schema import NotificationSchema, NotificationLogSchema
+from amanuensis.resources.notification import update_users_notifications, get_all_notification_log
 from cdislogging import get_logger
 
 logger = get_logger(__name__)
@@ -30,20 +30,16 @@ def retrieve_notifications():
         return notification_schema.dump(messages)
 
 
-blueprint_all = flask.Blueprint("all_notifications", __name__)
-
-@blueprint_all.route("/", methods=["GET"])
-def retrive_all_notifications():
+@blueprint_all.route("/all", methods=["GET"])
+def retrive_all_notification_log():
     try:
         logged_user_id = current_user.id
     except AuthError:
         logger.warning(
             "Unable to load or find the user, check your token"
         )
-        return flask.jsonify({"error": "Unauthorized"}), 401 
     with flask.current_app.db.session as session:
-        notification_schema = NotificationSchema(many=True)
-        messages = get_all_notifications(session, logged_user_id)
-        session.commit()
-        return notification_schema.dump(messages)
+        notification_log_schema = NotificationLogSchema(many=True)
+        messages = get_all_notification_log(session)
+        return notification_log_schema.dump(messages)
 
