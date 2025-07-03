985041a06df04c1b7dfb01fb4b8a64f52c86f3f0
Rachel Liu <hi.rachelliu@gmail.com>
2025-07-03 16:31:49 -0400

    join notification_log and notification together

--- amanuensis/blueprints/notification.py
+++ amanuensis/blueprints/notification.py
@@ -4,8 +4,8 @@ from amanuensis.auth.auth import current_user
 from amanuensis.errors import AuthError
 from amanuensis.schema import NotificationSchema, NotificationLogSchema
 from amanuensis.resources.notification import update_users_notifications
-from amanuensis.resources.userdatamodel.notification import get_notifications
 from cdislogging import get_logger
+from amanuensis.models import Notification, NotificationLog
 
 logger = get_logger(__name__)
 
@@ -40,7 +40,23 @@ def retrive_all_notification_log():
             "Unable to load or find the user, check your token"
         )
     with flask.current_app.db.session as session:
-        notification_schema = NotificationSchema(many=True)
-        all_messages = get_notifications(session, user_id = logged_user_id)
-        return notification_schema.dump(all_messages)
-
+        notifications_with_logs = (
+            session.query(Notification, NotificationLog)
+            .join(NotificationLog, Notification.notification_log_id == NotificationLog.id)
+            .filter(Notification.user_id == logged_user_id)
+            .order_by(Notification.create_date.desc())
+            .all()
+        )
+        notification_logs = []
+        for notification, log in notifications_with_logs:
+          notification_logs.append(
+              {
+              "notification_id":notification.id,
+              "user_id":notification.user_id,
+              "seen":notification.seen,
+              "date":notification.create_date.isoformat() if log.create_date else 'N/A',
+              "message":log.message,
+              "expiration_date":log.expiration_date.isoformat() if log.expiration_date else 'N/A'
+              }
+          )
+        return flask.jsonify(notification_logs)
