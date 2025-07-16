92f760af208f2254d6784f10af43484ff91007f1
Luca Graglia <graglia01@gmail.com>
2025-07-15 21:04:45 -0500

    import correct schema

--- amanuensis/blueprints/notification.py
+++ amanuensis/blueprints/notification.py
@@ -2,7 +2,7 @@ import flask
 
 from amanuensis.auth.auth import current_user
 from amanuensis.errors import AuthError
-from amanuensis.schema import NotificationSchema, NotificationLogSchema
+from amanuensis.schema import NotificationLogSchema
 from amanuensis.resources.notification import update_users_notifications
 from amanuensis.resources.userdatamodel.notification import get_notifications
 from amanuensis.resources.userdatamodel.notification_log import get_notification_logs
@@ -23,13 +23,13 @@ def retrieve_notifications():
             "Unable to load or find the user, check your token"
         )
     with flask.current_app.db.session as session:
-        notification_schema = NotificationSchema(many=True)
+        notificationlog_schema = NotificationLogSchema(many=True)
         
         messages = update_users_notifications(session, logged_user_id)
 
         session.commit()
     
-        return notification_schema.dump(messages)
+        return notificationlog_schema.dump(messages)
 
 
 @blueprint.route("/all", methods=["GET"])
